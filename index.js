dc95e5bd89a2d6a87962450e3828593b47edf0f1
Simon <simonwarui93@gmail.com>
2025-03-13 17:28:07 +0300

    assignmment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+function saturdayFun(game = "roller-skate"){
+    return`This Saturday, I want to ${game}!`
+}
+
+saturdayFun()
+saturdayFun("Go running")
+
+function mondayWork(todo = "go to the office"){
+    return `This Monday, I will ${todo}.`
+}
+
+function wrapAdjective(wrapper = "*"){
+    return function(adjective){
+        return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+
+}
